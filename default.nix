31c57b9acf4df4f3a59214320215bba8f5b941ae
Taylor Nekroze Lawson <nekroze.lawson@gmail.com>
2018-10-03 04:28:04 +1000

    Parameterize neovim vs vim usage

--- default.nix
+++ default.nix
@@ -1,13 +1,14 @@
-{ desktop ? true }:
+{ desktop ? true, neovim ? true }:
 
 { config, pkgs, lib, ... }:
 with lib;
 let
   gui = import ./gui.nix;
+  editor = if neovim then ./vim/neovim.nix else ./vim/vim.nix;
 in {
   imports = [
     ./tui.nix
-    ./vim/neovim.nix
+    editor
     (gui {
       desktop=desktop;
     })
