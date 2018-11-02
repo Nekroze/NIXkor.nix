d40c55056efe1014d9c9a857652cfebb8bc115a6
Taylor Nekroze Lawson <taylorl@benon.com>
2018-11-03 05:12:40 +1000

    Use vim as a pager

--- vim/neovim.nix
+++ vim/neovim.nix
@@ -5,4 +5,6 @@ let
 in {
   environment.systemPackages = neovimPackages;
   environment.variables.EDITOR = mkForce "nvim";
+  environment.variables.PAGER = mkForce "nvim -c 'set ro' -R";
+  environment.variables.MANPAGER = mkForce "nvim -c 'set ft=man ro nomod nolist nonu noma' -";
 }
--- vim/vim.nix
+++ vim/vim.nix
@@ -1,7 +1,10 @@
 { pkgs, ...}:
+with lib;
 let
   vimPackages = import ./vimPackages.nix pkgs;
 in {
   environment.systemPackages = vimPackages;
   environment.variables.EDITOR = mkForce "vim";
+  environment.variables.PAGER = mkForce "vim -c 'set ro' -R";
+  environment.variables.MANPAGER = mkForce "vim -c 'set ft=man ro nomod nolist nonu noma' -";
 }
