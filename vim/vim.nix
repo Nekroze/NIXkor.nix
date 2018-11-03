1ea8990aa813097e3af42cf49633cb2f5344091e
Taylor Nekroze Lawson <taylorl@benon.com>
2018-11-04 08:29:23 +1000

    Use -R switch when neovim read only mode is desired

--- vim/vim.nix
+++ vim/vim.nix
@@ -5,6 +5,6 @@ let
 in {
   environment.systemPackages = vimPackages;
   environment.variables.EDITOR = mkForce "vim";
-  environment.variables.PAGER = mkForce "vim -c 'set ro' -R";
-  environment.variables.MANPAGER = mkForce "vim -c 'set ft=man ro nomod nolist nonu noma' -";
+  environment.variables.PAGER = mkForce "vim -R";
+  environment.variables.MANPAGER = mkForce "vim -R -c 'set ft=man nomod nolist nonu noma' -";
 }
